c83a1cf961a7115aead0ae9187feb995aef8c01c
Steven J. Selcuk <stevenjselcuk@gmail.com>
2021-02-27 18:32:53 +0300

    Initial commit

---
+++ TabbyNote/AppDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  AppDelegate.swift
+//  Atomic Note
+//
+//  Created by Steven J. Selcuk on 27.02.2021.
+//
+
+import Cocoa
+import SwiftUI
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+
+        if let button = statusBarItem.button {
+        //    button.image = NSImage(named: "menubar-icon") No icon. Just text.
+            button.imagePosition = NSControl.ImagePosition.imageLeft
+            button.title = UserDefaults.standard.optionalString(forKey: "note") ?? "🐈 TabbyNote - Click here to change"
+            button.font = NSFont.monospacedDigitSystemFont(ofSize: 12.0, weight: NSFont.Weight.bold)
+            button.action = #selector(togglePopover(_:))
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    @objc func togglePopover(_ sender: AnyObject?) {
+        let event = NSApp.currentEvent!
+
+        if event.type == NSEvent.EventType.rightMouseUp {
+        } else if event.type == NSEvent.EventType.leftMouseUp {
+            let menu = NSMenu()
+            menu.addItem(withTitle: "Copy", action: #selector(copyIt), keyEquivalent: "c")
+            menu.addItem(withTitle: "Edit", action: #selector(edit), keyEquivalent: "e")
+            menu.addItem(NSMenuItem.separator())
+            menu.addItem(withTitle: "Quit App", action: #selector(quit), keyEquivalent: "q")
+            statusBarItem.menu = menu
+            statusBarItem.button?.performClick(nil)
+            statusBarItem.menu = nil
+        }
+    }
+
+    @objc func copyIt() {
+        let note = UserDefaults.standard.optionalString(forKey: "note") ?? "AtomicNote"
+        let pasteboard = NSPasteboard.general
+        pasteboard.clearContents()
+        pasteboard.setString(note, forType: NSPasteboard.PasteboardType.string)
+        NSSound(named: "Pop")?.play()
+    }
+
+    @objc func edit() {
+        EditWindowController().showWindow()
+    }
+
+    @objc func quit() {
+        NSApp.terminate(self)
+    }
+
+    func updateTitle(newTitle: String) {
+        statusBarItem.button?.title = newTitle
+    }
+
+}
---
+++ TabbyNote/Edit.swift
@@ -0,0 +1,121 @@
+//
+//  Edit.swift
+//  Atomic Note
+//
+//  Created by Steven J. Selcuk on 27.02.2021.
+//
+
+import Cocoa
+import SwiftUI
+
+let settings = UserDefaults.standard
+
+extension UserDefaults {
+    public func optionalString(forKey defaultName: String) -> String? {
+        let defaults = self
+        if let value = defaults.value(forKey: defaultName) {
+            return value as? String
+        }
+        return nil
+    }
+}
+
+final class MakeWindow: NSWindow, NSWindowDelegate {
+    convenience init() {
+        self.init(
+            contentRect: .zero,
+            styleMask: [
+                .titled,
+                .closable,
+                .miniaturizable,
+                .resizable,
+            ],
+            backing: .buffered,
+            defer: true
+        )
+
+        titlebarAppearsTransparent = true
+        delegate = self
+    }
+}
+
+struct EditView: View {
+    var nsObject: AnyObject? = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as AnyObject
+    @State var note: String = UserDefaults.standard.optionalString(forKey: "note") ?? "🐈 TabbyNote - Click here to change"
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            TextEditor(text: $note)
+                .cornerRadius(9)
+                .font(.body)
+                .lineSpacing(1)
+                .padding(.all, 5.0)
+                .onChange(of: note) { _ in
+                    settings.set(note, forKey: "note")
+                    (NSApp.delegate as! AppDelegate).updateTitle(newTitle: String(note))
+                    if note.isEmpty {
+                        settings.set("🐈 TabbyNote", forKey: "note")
+                        (NSApp.delegate as! AppDelegate).updateTitle(newTitle: String("🐈 TabbyNote"))
+                    }
+                }
+                .padding(.all, 10.0)
+            HStack {
+                HStack {
+                    Text("Bug or Feature?")
+
+                    Button(action: {
+                        let email = "https://twitter.com/TabbyCatLLC"
+                        if let url = URL(string: email) {
+                            NSWorkspace.shared.open(url)
+                        }
+                    }) {
+                        Text("Tell Me")
+                    }
+                }
+
+                HStack {
+                    Text("Do you like it?")
+
+                    Button(action: self.requestReviewManually) {
+                        Text("5 X ⭐️")
+                    }
+                }
+            }
+            .padding(.bottom, 20.0)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+    }
+
+    func requestReviewManually() {
+        guard let writeReviewURL = URL(string: "https://apps.apple.com/app/id1543879930?action=write-review")
+        else { fatalError("Expected a valid URL") }
+        NSWorkspace.shared.open(writeReviewURL)
+    }
+}
+
+final class EditWindowController: NSWindowController {
+    convenience init() {
+        let window = MakeWindow()
+        self.init(window: window)
+
+        window.contentView = NSHostingView(rootView: EditView())
+
+        window.title = "🐈 TabbyNote - Edit Note"
+        window.styleMask = [
+            .titled,
+            .closable,
+            .borderless,
+        ]
+        var windowFrame = window.frame
+        windowFrame.size = NSMakeSize(500, 160)
+
+        window.setFrame(windowFrame, display: true, animate: true)
+        window.level = .floating
+        window.titlebarAppearsTransparent = true
+        window.center()
+    }
+
+    func showWindow() {
+        NSApp.activate(ignoringOtherApps: true)
+        window?.makeKeyAndOrderFront(nil)
+    }
+}
